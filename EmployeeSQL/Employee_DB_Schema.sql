94e9db52e5325c3ec2d13984dd25d2cb5fdad992
szooberry <sameer.zubairi@gmail.com>
2023-03-04 18:30:50 -0500

    Added comments for more clarity

--- EmployeeSQL/Employee_DB_Schema.sql
+++ EmployeeSQL/Employee_DB_Schema.sql
@@ -1,13 +1,16 @@
-﻿CREATE TABLE departments(
+﻿-- Create Table called departments for departments.csv
+CREATE TABLE departments(
 	dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
 	dept_name VARCHAR(250) NOT NULL
 );
 
+-- Create Table called titles for titles.csv
 CREATE TABLE titles(
 	title_id VARCHAR(10) PRIMARY KEY NOT NULL,
 	title VARCHAR(250) NOT NULL
 );
 
+-- Create Table called employees for employees.csv
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY NOT NULL,
 	emp_title_id VARCHAR(10) NOT NULL,
@@ -19,6 +22,7 @@ CREATE TABLE employees(
 	hire_date VARCHAR(20)
 );
 
+-- Create Table called departmentemployees for dept_emp.csv
 CREATE TABLE departmentemployees(
 	emp_no INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
@@ -26,6 +30,7 @@ CREATE TABLE departmentemployees(
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
+-- Create Table called departmentmanagers for dept_manager.csv
 CREATE TABLE departmentmanagers(
 	dept_no VARCHAR(10) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
@@ -33,6 +38,7 @@ CREATE TABLE departmentmanagers(
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+-- Create Table called salaries for salaries.csv
 CREATE TABLE salaries(
 	emp_no INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
