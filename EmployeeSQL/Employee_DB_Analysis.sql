b0541c85e12a8a243804e2d0710ef2d2fce930e4
szooberry <sameer.zubairi@gmail.com>
2023-03-04 18:53:17 -0500

    last minute changes

--- EmployeeSQL/Employee_DB_Analysis.sql
+++ EmployeeSQL/Employee_DB_Analysis.sql
@@ -84,7 +84,8 @@ WHERE departmentemployees.emp_no IN
 		WHERE dept_name = 'Sales' OR 
 		dept_name = 'Development'
 		)
-	);
+	)
+AND departments.dept_name IN ('Sales', 'Development');
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(last_name) AS "Frequency Count"
